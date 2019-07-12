5d75d4fb1c33ecb90eb2fd630402d3f9abcd94c5
Sumit Kumar <sumitkumar97.official@gmail.com>
2019-07-12 20:15:42 +0530

    Refactored code

--- album/urls.py
+++ album/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('add', FileUploadView.as_view()), #add image to the album   POST   not working for me
+    path('add', FileUploadView.as_view()), #add image to the album   POST
     path('create', AlbumView.as_view()), #create new album   POST
     path('list/private',AlbumListPrivate.as_view()), #get private + public albums GET
     path('list/public',AlbumListPublic.as_view()), #get public albums  GET
-    path('<int:album_id>/images',AlbumImageList.as_view()), #GET list album images
-    path('delete/<int:album_id>',AlbumDeleteView.as_view()), #GET delete album
-    path('delete/image/<int:image_id>',ImageDeleteView.as_view()) #GET delete image
+    path('<int:album_id>/images',AlbumImageList.as_view()), #list album images  GET
+    path('delete/<int:album_id>',AlbumDeleteView.as_view()), #delete album  GET
+    path('delete/image/<int:image_id>',ImageDeleteView.as_view()) #delete image  GET
 ]
