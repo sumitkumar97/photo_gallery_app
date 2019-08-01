e21465a2637094b46b2d6c7eca3b005682aa917e
Sumit Kumar <sumitkumar97.official@gmail.com>
2019-08-01 20:31:05 +0530

    Album page for Trending Albums

--- ui/src/Trending.js
+++ ui/src/Trending.js
@@ -13,6 +13,7 @@ class Trending extends React.Component {
         };
 
         this.fetchAlbums = this.fetchAlbums.bind(this);
+        this.deleteAlbums = this.deleteAlbums.bind(this);
     }
 
     componentWillMount(){
@@ -42,12 +43,49 @@ class Trending extends React.Component {
         });
     }
 
+    async deleteAlbums(albumId){
+        let url = `${API_HOST}/api/v1/album/delete/${albumId}`;
+        let { token } = this.state;
+        let selfReference = this ;
+        await fetch(url, {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                //'Content-Type': 'application/json',
+                'Authorization': `TOKEN ${token}`,
+            }
+        })
+        .then(function(response) {
+            selfReference.fetchAlbums();
+            return response;
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
     render() {
         let {albumList} = this.state;
+        let selfReference = this ;
         let albumListJsx = albumList.map(
             function(album){
                 return (<div key={album.id} className='flexdiv-vertical'>
-                            <h3>{album.name}</h3>
+                            <div style={{'display':'flex'}}>
+                                <u>
+                                    <h3
+                                        className="click-heading"
+                                        onClick={()=>selfReference.props.changeAppState(
+                                            {
+                                                albumId: album.id,
+                                                albumName: album.name,
+                                                albumOwner: false,
+                                                imageActiveItem: 'images',
+                                                appState: 'imagesPage',
+                                            }
+                                        )}
+                                    >
+                                        {album.name}
+                                    </h3>
+                                </u>
+                                <button className="negative ui button" onClick={()=>selfReference.deleteAlbums(album.id)}>Delete</button>
+                            </div>
                             <div>{`By ${album.owner}`}</div>
                             { album.cover ?
                                 <img className="ui small image" style={{'width': '500px'}} src={API_HOST+album.cover} alt='Not available'/>
