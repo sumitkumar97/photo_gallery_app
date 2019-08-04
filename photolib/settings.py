4ef3a81c7138e60c1e058b3c1fd5b6b38790ecf4
Sumit Kumar <sumitkumar97.official@gmail.com>
2019-08-04 10:45:51 +0530

    Modifies cors origin whitelist

--- photolib/settings.py
+++ photolib/settings.py
@@ -29,9 +29,8 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOW_CREDENTIALS = True
 
-#CORS_ORIGIN_ALLOW_ALL=True
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+CORS_ORIGIN_ALLOW_ALL=True
+#CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
 
 # Application definition
 
